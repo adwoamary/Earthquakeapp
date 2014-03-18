4f8b069ada5e535e6998ad65deb8a360274ec592
kachikk <onyekachikk@yahoo.com>
2014-03-18 06:35:32 +0000

    creating-pages

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,20 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Earth::Application.config.secret_key_base = '6105211994566711e59a32af6f72677c9d0dc04af742cbff75d8948c51d57c1879654a7dcdc7de00f7207d43454b1e2dbc291155dc2e0e2be2828eaaf48e7e58'
+#Earth::Application.config.secret_key_base = #'6105211994566711e59a32af6f72677c9d0dc04af742cbff75d8948c51d57c1879654a7dcdc7de00f7207d43454b1e2dbc291155dc2e0e2be2828eaaf48e7e58'
+require 'securerandom'
+
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+
+Earth::Application.config.secret_key_base = secure_token
