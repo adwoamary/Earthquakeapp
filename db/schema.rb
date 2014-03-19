b1d39362492c8350905b4f3bbcf6f73e6e46c6f9
adwoamary <mary_boamah200@yahoo.co.uk>
2014-03-19 23:32:26 +0000

    User Page

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140319151542) do
+ActiveRecord::Schema.define(version: 20140319223927) do
 
   create_table "stations", force: true do |t|
     t.text     "trading_name"
--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,8 @@
 FactoryGirl.define do
   factory :user do
-    name     "Michael Hartl"
-    email    "michael@example.com"
-    password "foobar"
-    password_confirmation "foobar"
+    name     "Mary Boamah"
+    email    "mary@example.com"
+    password "candyman"
+    password_confirmation "candyman"
   end
 end
