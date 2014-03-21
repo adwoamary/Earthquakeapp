ff92179188ea77d0bf19f2230cb3fe1b24655815
kachikk <onyekachikk@yahoo.com>
2014-03-21 00:19:03 +0000

    rspec-tests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,11 @@
 class ApplicationController < ActionController::Base
-<<<<<<< HEAD
-  protect_from_forgery
+#<<<<<<< HEAD
+ # protect_from_forgery
  
-=======
+#=======
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
    include SessionsHelper
->>>>>>> a7535958f0ce70fa09ed9dcd76d632600b1809e8
+#>>>>>>> a7535958f0ce70fa09ed9dcd76d632600b1809e8
 end
